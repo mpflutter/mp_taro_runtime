6b6bc50900ed370ef17226321ca4ff1644380b5e
崔 明辉 <cuis@vip.qq.com>
2021-02-10 12:47:20 +0800

    fix: Optimize text measure speed.

--- src/components/mpcore/text_measurer.tsx
+++ src/components/mpcore/text_measurer.tsx
@@ -9,20 +9,28 @@ export class TextMeasurer extends React.Component<
   {
     scaffold: MPComponentsProps;
   },
-  { targetTexts: MPComponentsProps[] }
+  { targetTexts: MPComponentsProps[]; targetHash: string }
 > {
-  static scanRichText(data: MPComponentsProps, result: MPComponentsProps[]) {
+  static scanRichText(
+    data: MPComponentsProps,
+    result: MPComponentsProps[],
+    resultHash: string[]
+  ) {
     if (!data) return;
     if (data.name === "rich_text" && data.attributes.measureId) {
       result.push(data);
+      resultHash.push(data.attributes.measureId);
     } else {
       data.children?.forEach((it) => {
-        this.scanRichText(it, result);
+        this.scanRichText(it, result, resultHash);
       });
     }
   }
 
-  state: { targetTexts: MPComponentsProps[] } = { targetTexts: [] };
+  state: { targetTexts: MPComponentsProps[]; targetHash: string } = {
+    targetTexts: [],
+    targetHash: "",
+  };
 
   componentDidMount() {
     this.doScan(this.props);
@@ -34,34 +42,42 @@ export class TextMeasurer extends React.Component<
 
   componentDidUpdate() {
     if (!this.state.targetTexts.length) return;
-    setTimeout(() => {
-      Taro.createSelectorQuery()
-        .selectAll(".text_measure_item")
-        .fields({ size: true, id: true }, (res) => {
-          App.callbackChannel(
-            JSON.stringify({
-              type: "rich_text",
-              message: {
-                event: "onMeasured",
-                data: res.map((it) => {
-                  return {
-                    measureId: parseInt(it.id?.replace("text_measurer_", "")),
-                    size: { width: it.width, height: it.height },
-                  };
-                }),
-              },
-            })
-          );
-        })
-        .exec();
-    }, 0);
+    Taro.createSelectorQuery()
+      .selectAll(".text_measure_item")
+      .fields({ size: true, id: true }, (res) => {
+        App.callbackChannel(
+          JSON.stringify({
+            type: "rich_text",
+            message: {
+              event: "onMeasured",
+              data: res.map((it) => {
+                return {
+                  measureId: parseInt(it.id?.replace("text_measurer_", "")),
+                  size: { width: it.width, height: it.height },
+                };
+              }),
+            },
+          })
+        );
+      })
+      .exec();
   }
 
   doScan(props: { scaffold: MPComponentsProps }) {
     let result: MPComponentsProps[] = [];
-    TextMeasurer.scanRichText(props.scaffold?.attributes?.body, result);
+    let resultHash: string[] = [];
+    TextMeasurer.scanRichText(
+      props.scaffold?.attributes?.body,
+      result,
+      resultHash
+    );
+    let targetHash = resultHash.join(",");
+    if (targetHash === this.state.targetHash) {
+      return;
+    }
     this.setState({
       targetTexts: result,
+      targetHash,
     });
   }
 
